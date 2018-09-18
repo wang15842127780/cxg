3dbf8541894e0871d459396897ee9ef8a830770b
wang15842127780 <root@localhost.localdomain>
2018-09-18 10:35:20 +0800

    [

--- server.js
+++
@@ -1,8 +0,0 @@
-var WebSocketServer = require('ws').Server;
-var wss = new WebSocketServer({"http://47.104.93.214:8888"});
-wss.on('connection',function(ws){
-	console.log('client connected');
-	ws.on('message',function(message){
-		console.log('message');
-	})
-});
